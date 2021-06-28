d2dcea65200e4d494b3a867ec2b231612464f44c
JiHwonChoi <86228531+JiHwonChoi@users.noreply.github.com>
2021-06-28 18:19:12 +0900

    Delete 45.py

--- 0628/45.py
+++
@@ -1,9 +0,0 @@
-class Korean:
-    def printNationality() :
-        print ('대한민국')
-        
-    def __init__(self) -> None:
-        self.printNationality()
-        
-a=Korean()
-a.printNationality()
