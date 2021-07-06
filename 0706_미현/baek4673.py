e01f8cad8e60e7d246fb3010b7ca9196e3c748e4
curin <curinglee@gmail.com>
2021-07-06 22:17:56 +0900

    백준 4673 문제 스터디 시간에 클래스로 바꿈

--- 0706_미현/baek4673.py
+++ 0706_미현/baek4673.py
@@ -1,18 +1,25 @@
-def d(n):
-    a = 0
-    for value in list(str(n)):
-        a = a + int(value)
-        # print('n 값: {}, value 값: {}, a 값: {}'.format(n, value, a))
-    return int(n) + a
+class boj4673():
+    def __init__(self):
+        self._produce_num = []
 
+    def d(n):
+        a = 0
+        for value in list(str(n)):
+            a = a + int(value)
+            # print('n 값: {}, value 값: {}, a 값: {}'.format(n, value, a))
+        return int(n) + a
 
-produce_num = []
+    def count(self):
+        for count in range(1, 10001):
+            self.produce_num.append(self.d(count))
 
-for count in range(1, 10001):
-    produce_num.append(d(count))
+    def printd(self):
+        for i in range(1, 10000):
+            if i in self.produce_num:
+                continue
+            else:
+                print(i)
 
-for i in range(1, 10000):
-    if i in produce_num:
-        continue
-    else:
-        print(i)
+b=boj4673()
+b.count()
+b.printd()
